82e1472d4b7b66d10f40de2f69beb7ad86639726
Alexander López <lopez.manrique.alexander@gmail.com>
2022-09-12 11:26:35 -0500

    Update tb3_waffle.sh

--- tb3_waffle.sh
+++ tb3_waffle.sh
@@ -1,7 +1,7 @@
 echo "1. Installing ROS Noetic"
 sudo apt update
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt install curl
+sudo apt -y install curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
 sudo apt -y install ros-noetic-ros-base
