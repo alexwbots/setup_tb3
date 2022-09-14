533f4980c3d927fdbc45a0ae9b821e18d67f387d
Alexander López <lopez.manrique.alexander@gmail.com>
2022-09-14 12:41:31 -0500

    Update tb3_waffle.sh

--- tb3_waffle.sh
+++ tb3_waffle.sh
@@ -13,16 +13,16 @@ source ~/.bashrc
 
 echo "2. Installing ROS packages for tb3"
 sudo apt-get -y install ros-noetic-joy ros-noetic-teleop-twist-joy \
-  ros-noetic-teleop-twist-keyboard ros-noetic-laser-proc \
-  ros-noetic-rgbd-launch ros-noetic-rosserial-arduino \
-  ros-noetic-rosserial-python ros-noetic-rosserial-client \
-  ros-noetic-rosserial-msgs ros-noetic-amcl ros-noetic-map-server \
-  ros-noetic-move-base ros-noetic-urdf ros-noetic-xacro \
-  ros-noetic-compressed-image-transport ros-noetic-rqt* ros-noetic-rviz \
-  ros-noetic-gmapping ros-noetic-navigation ros-noetic-interactive-markers \
-  ros-noetic-dynamixel-sdk ros-noetic-turtlebot3-msgs ros-noetic-turtlebot3 \
-  ros-noetic-usb-cam ros-noetic-visp ros-noetic-visp-auto-tracker ros-noetic-image-view \
-  ros-noetic-hls-lfcd-lds-driver
+ros-noetic-teleop-twist-keyboard ros-noetic-laser-proc \
+ros-noetic-rgbd-launch ros-noetic-rosserial-arduino \
+ros-noetic-rosserial-python ros-noetic-rosserial-client \
+ros-noetic-rosserial-msgs ros-noetic-amcl ros-noetic-map-server \
+ros-noetic-move-base ros-noetic-urdf ros-noetic-xacro \
+ros-noetic-compressed-image-transport ros-noetic-rqt* ros-noetic-rviz \
+ros-noetic-gmapping ros-noetic-navigation ros-noetic-interactive-markers \
+ros-noetic-dynamixel-sdk ros-noetic-turtlebot3-msgs ros-noetic-turtlebot3 \
+ros-noetic-usb-cam ros-noetic-visp ros-noetic-visp-auto-tracker ros-noetic-image-view \
+ros-noetic-hls-lfcd-lds-driver
   
 echo "3. Parameters for tb3"
 echo "export TURTLEBOT3_MODEL=waffle_pi" >> ~/.bashrc
